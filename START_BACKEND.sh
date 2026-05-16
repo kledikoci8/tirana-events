8fe5b5f222cbab5388a310dd3e817735a170275b
Kledi Koci <kledikoci8@gmail.com>
2026-05-16 23:22:55 +0200

    Remove shell script files (.sh)

--- START_BACKEND.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "🚀 Starting Tirana Events Backend..."
-echo ""
-cd backend
-./mvnw spring-boot:run
--- START_MOBILE.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "📱 Starting Tirana Events Mobile App..."
-echo ""
-cd mobile
-npm start
